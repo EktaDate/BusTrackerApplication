40eda04bb55063184a0417225db3a5add82701db
Rupali <rupali@mastek.com>
2018-06-11 14:11:42 +0530

    second commit

--- BusTrackerApplication/App_Start/RouteConfig.cs
+++ BusTrackerApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BusTrackerApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "SignIn", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "HomePage", id = UrlParameter.Optional }
             );
         }
     }
